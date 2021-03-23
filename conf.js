f603a508630648b47af12048dfdf04e04bd0021c
kuang-koki <kuang-koki@users.noreply.github.com>
2021-03-23 19:01:52 +0100

    login testing V1.0
    login testing is finished

--- conf.js
+++ conf.js
@@ -2,6 +2,7 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs:['spec.js'],
 	onPrepare: function() {
-  browser.manage().window().setSize(1920, 1080);
+  //browser.manage().window().setSize(1920, 1080);
+  browser.driver.manage().window().maximize();
 }
 };
